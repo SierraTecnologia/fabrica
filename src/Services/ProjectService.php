8fff54521fe0e58ebec3a54283b53555159a3ec3
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-06-15 13:54:03 -0300

    RP-344 - GEradores de Modelos com base em bancos prontos

---
+++ src/Services/ProjectService.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @todo
+ */
+namespace Fabrica\Services;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+use Fabrica\Repositories\ProjectRepository;
+use Fabrica\Models\Project;
+use Muleta\Utils\Modificators\StringModificator;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectService
+{
+    public function __construct(
+        ProjectRepository $projectRepository
+    ) {
+        $this->repo = $projectRepository;
+    }
+
+    /**
+     * @todo Terminar de Fazer
+     */
+    public static function import($data)
+    {   
+        $registerData = $data;
+        // if (isset($data['Nome Completo'])) {
+        //     $registerData['name'] = $data["Nome Completo"];
+        // }
+        // if (isset($data['CPF'])) {
+        //     $registerData['cpf'] = $data["CPF"];
+        // }
+        // if (isset($data['Nascimento'])) {
+        //     $registerData['birthday'] = $data["Nascimento"];
+        // }
+        $code = $registerData['code'] = StringModificator::cleanCodeSlug($registerData['name']);
+
+        if (Project::find($code)) {
+            return true;
+        }
+        $project = Project::createIfNotExistAndReturn($registerData);
+        return true;
+    }
+}
