ea3eb755b8e1d127c30d9431faa890d9a68d30b3
Ricardo R. Sierra <sierra.csi@gmail.com>
2022-06-22 14:33:17 -0300

    COmmit

--- src/Http/Controllers/ProjectController.php
+++ src/Http/Controllers/ProjectController.php
@@ -93,7 +93,7 @@ class ProjectController extends Base
     // /**
     //  * Display all the workers
     //  *
-    //  * @return Illuminate\View\View
+    //  * @return \Illuminate\View\View
     //  */
     // public function index(Request $request)
     // {
